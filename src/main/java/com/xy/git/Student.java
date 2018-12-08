7b8fdcea9b33f084ffc88e5b495bcec9c277b2b0
qqg <353213432@qq.com>
2018-12-08 21:03:17 +0800

    second commit

--- src/main/java/com/xy/git/Student.java
+++ src/main/java/com/xy/git/Student.java
@@ -1,5 +1,27 @@
 package com.xy.git;
 
 public class Student {
-
+	
+	private long id;
+	private String name;
+	private String idCard;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIdCard() {
+		return idCard;
+	}
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+	
 }
